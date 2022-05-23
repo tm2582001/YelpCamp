d0069c01e6674f0d881d59c85cb7275ab10cd450
Tushar <tm2582001@gmail.com>
2022-05-23 21:41:14 +0530

    fix error

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
 
-const dbUrl = /*process.env.DB_URL ||*/ "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
